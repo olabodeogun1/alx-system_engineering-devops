df19cdf0097fd7ac33c6195aa999e8b0156a698f
Olabode Ogun <olabodeogun1@gmail.com>
2023-12-05 15:42:35 -0800

    alx-system_engineering-devops

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,9 @@
-# Custom HTTP header in a nginx server
-
-# update ubuntu server
-exec { 'update server':
-  command  => 'apt-get update',
-  user     => 'root',
-  provider => 'shell',
-}
-->
-# install nginx web server on server
-package { 'nginx':
-  ensure   => present,
-  provider => 'apt'
-}
-->
-# custom Nginx response header (X-Served-By: hostname)
-file_line { 'add HTTP header':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;'
-}
-->
-# start service
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx']
+# Automate the task of creating a custom HTTP header response, but with Puppet.
+exec { 'command':
+  command => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
 
